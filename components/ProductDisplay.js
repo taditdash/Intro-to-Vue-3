b2563b01dd02f71afab27e184c5b0e458f03ddd6
Tadit Dash <taditdash@gmail.com>
2022-12-13 11:00:23 +0530

    Add codes manage cart feature from main.js.

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -39,6 +39,13 @@ app.component('product-display', {
           v-on:click="addToCart">
           Add to Cart
         </button>
+        <button 
+          class="button" 
+          :class="{ disabledButton: !inStock }" 
+          :disabled="!inStock" 
+          v-on:click="removeFromCart">
+          Remove from Cart
+        </button>
       </div>
     </div>
   </div>`,
@@ -56,21 +63,24 @@ app.component('product-display', {
   },
   methods: {
       addToCart() {
-          this.cart += 1
+        this.$emit('add-to-cart', this.variants[this.selectedVariant].id);
+      },
+      removeFromCart() {
+        this.$emit('remove-from-cart', this.variants[this.selectedVariant].id);
       },
       updateVariant(index) {
-          this.selectedVariant = index
+        this.selectedVariant = index
       }
   },
   computed: {
       title() {
-          return this.brand + ' ' + this.product
+        return this.brand + ' ' + this.product
       },
       image() {
-          return this.variants[this.selectedVariant].image
+        return this.variants[this.selectedVariant].image
       },
       inStock() {
-          return this.variants[this.selectedVariant].quantity
+        return this.variants[this.selectedVariant].quantity
       },
       shipping() {
         if (this.premium) {
